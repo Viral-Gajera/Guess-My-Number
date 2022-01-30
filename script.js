efc7ddb3e5120407f5b194d07ca2ba2c53d2accd
Viral Gajera <viral.gajera218@gmail.com>
2022-01-30 17:10:10 +0530

    input type=number selected in js file

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ let check = document.getElementsByClassName('button')[1];
 let scrore = document.getElementById('score');
 let highscore = document.getElementById('highscore');
 let secretNum = document.getElementById('secret-num');
+let input = document.getElementById('input');
