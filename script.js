1b13cdc5d3e8c098a9f382d68ae51af68ae103e1
Viral Gajera <viral.gajera218@gmail.com>
2022-01-30 17:06:08 +0530

    html, css, js all file updated
    message, score, and high-score class added in index.html, button effect added in style.css, all required elements are selected in script.js file

---
+++ script.js
@@ -0,0 +1,6 @@
+let message = document.querySelector('.message');
+let again = document.getElementsByClassName('button')[0];
+let check = document.getElementsByClassName('button')[1];
+let scrore = document.getElementById('score');
+let highscore = document.getElementById('highscore');
+let secretNum = document.getElementById('secret-num');
