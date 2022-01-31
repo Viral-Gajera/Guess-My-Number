f020be389e3a0dc53718ee62e102072325d65ccf
Viral Gajera <viral.gajera218@gmail.com>
2022-02-01 00:04:47 +0530

    event listener added to check button

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-let message = document.querySelector('.message');
 let again = document.getElementsByClassName('button')[0];
 let check = document.getElementsByClassName('button')[1];
+let message = document.querySelector('.message');
 let scrore = document.getElementById('score');
 let highscore = document.getElementById('highscore');
 let secretNum = document.getElementById('secret-num');
 let input = document.getElementById('input');
+let description = document.getElementById('description');
+
+
+const MINIMUM = 1;
+const MAXIMUM = 20;
+let RANDOM = MINIMUM +( Math.random() * (MAXIMUM - MINIMUM) + 1 ) ;
+RANDOM = Math.floor(RANDOM);
+console.log(RANDOM);
+
+description.innerHTML = `< Between ${MINIMUM} and ${MAXIMUM} >`
+
+check.addEventListener('click',function(){
+    let enteredValue = input.value;
+    console.log(enteredValue);
+
+    if(enteredValue == ''){
+        message.textContent = `> Invalid input!!`;
+    }
+    else{
+        if(enteredValue < RANDOM){
+            message.textContent = `> Too Low!!`;
+        }
+        else if(enteredValue > RANDOM ){
+            message.textContent = `> Too High!!`;
+        }
+        else{
+            message.textContent = `> Correct 🏆 !!`;
+        }
+    }
+});
